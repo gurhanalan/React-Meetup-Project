85a047e77752931fc2a705d60395e7264a3782f9
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-10-05 17:10:54 +0300

    Add links and pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Route, Switch } from "react-router-dom";
+import MainNavigation from "./components/MainNavigation";
+import AllMeetups from "./pages/AllMeetups";
+import Favorites from "./pages/Favorites";
+import NewMeetup from "./pages/NewMeetup";
+
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <MainNavigation />
+            <Switch>
+                <Route path="/favorites" component={Favorites} />
+                <Route path="/new-meetup" component={NewMeetup} />
+                <Route path="/" component={AllMeetups} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/MainNavigation.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./mainNavigation.module.css";
+
+const MainNavigation = () => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>React Meetups</div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">All Meetups</Link>
+                    </li>
+                    <li>
+                        <Link to="/new-meetup">Add New Meetup</Link>
+                    </li>
+                    <li>
+                        <Link to="/favorites">Favorites</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default MainNavigation;
