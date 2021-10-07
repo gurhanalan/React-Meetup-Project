25b7b4788f391a1b88d739794641dfc21d62a634
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-10-07 14:09:10 +0300

    Add fav. page

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import FavoritesContext from "../store/favorites-context";
 
 const Favorites = () => {
-    return <div>Favorites</div>;
+    const favoritesCtx = useContext(FavoritesContext);
+
+    let content;
+
+    if (favoritesCtx.totalFavorites === 0) {
+        content = <p>No Favorites yet. Try adding some.</p>;
+    } else {
+        content = <MeetupList meetups={favoritesCtx.favorites} />;
+    }
+
+    return (
+        <section>
+            <h1>Favorites</h1>
+            {content}
+        </section>
+    );
 };
 
 export default Favorites;
